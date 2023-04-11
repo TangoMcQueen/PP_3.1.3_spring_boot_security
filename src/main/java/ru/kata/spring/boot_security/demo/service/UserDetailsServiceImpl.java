b18f6401915ee597fdc176df6055fef8b33db752
Evgeniy Zorin <112500911+TangoMcQueen@users.noreply.github.com>
2023-04-11 18:54:08 +0300

    #1

---
+++ src/main/java/ru/kata/spring/boot_security/demo/service/UserDetailsServiceImpl.java
@@ -0,0 +1,33 @@
+package ru.kata.spring.boot_security.demo.service;
+
+import org.hibernate.Hibernate;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import ru.kata.spring.boot_security.demo.model.User;
+import ru.kata.spring.boot_security.demo.repository.UserRepository;
+
+import java.util.Optional;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private UserRepository userRepository;
+
+    public UserDetailsServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Optional<User> optionalUser = userRepository.findByUsername(username);
+        if (optionalUser.isEmpty()) {
+            throw new UsernameNotFoundException("User not Found!!!");
+        }
+        User user = optionalUser.get();
+        Hibernate.initialize(user.getRoles());
+        return user;
+    }
+}
---
+++ src/main/java/ru/kata/spring/boot_security/demo/service/UserServiceImp.java
@@ -0,0 +1,45 @@
+package ru.kata.spring.boot_security.demo.service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import ru.kata.spring.boot_security.demo.model.User;
+import ru.kata.spring.boot_security.demo.repository.UserRepository;
+
+
+import java.util.List;
+
+
+@Service
+public class UserServiceImp implements UserService{
+    private UserRepository userRepository;
+    public UserServiceImp(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+    @Transactional(readOnly = true)
+    @Override
+    public List<User> listUsers() {
+        return userRepository.findAll();
+    }
+    @Override
+    @Transactional
+    public User save(User user) {
+        return userRepository.save(user);
+    }
+    @Override
+    @Transactional(readOnly = true)
+    public User getUser(Long id) {
+        return userRepository.getById(id);
+    }
+    @Override
+    @Transactional
+    public void update(User user) {
+        userRepository.save(user);
+    }
+    @Override
+    @Transactional
+    public void delete(Long id) {
+        userRepository.deleteById(id);
+    }
+
+
+}
